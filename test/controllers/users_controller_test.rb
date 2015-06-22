acf5f5fe7e576c9f621ff3b685f3e7105518459e
Kyle Rand <krand7@gmail.com>
2015-06-21 21:23:46 -0400

    Updated tests to allow user authentication

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,27 +1,37 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+
+  setup do
+    @regular_user = users(:one)
+  end
+
   test "should get index" do
+    login(@regular_user)
     get :index
     assert_response :success
   end
 
   test "should get edit" do
+    login(@regular_user)
     get :edit
     assert_response :success
   end
 
   test "should get update" do
+    login(@regular_user)
     get :update
     assert_response :success
   end
 
   test "should get show" do
+    login(@regular_user)
     get :show
     assert_response :success
   end
 
   test "should get destroy" do
+    login(@regular_user)
     get :destroy
     assert_response :success
   end
