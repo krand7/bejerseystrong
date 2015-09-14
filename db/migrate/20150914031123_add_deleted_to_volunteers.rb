2fe49ae41b10bf5eaae8a61bf9d486213332e9ee
Kyle Rand <krand7@gmail.com>
2015-09-14 00:16:52 -0400

    New volunteers listed as current immediately

--- db/migrate/20150914031123_add_deleted_to_volunteers.rb
+++ db/migrate/20150914031123_add_deleted_to_volunteers.rb
@@ -1,5 +1,5 @@
 class AddDeletedToVolunteers < ActiveRecord::Migration
   def change
-    add_column :volunteers, :deleted, :boolean
+    add_column :volunteers, :deleted, :boolean, null: false, default: false
   end
 end
