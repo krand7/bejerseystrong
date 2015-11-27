2a2795c8c33e034a02a20759f7201bc3ed54cfc7
Kyle Rand <krand7@gmail.com>
2015-11-27 10:17:17 -0500

    Fixed user profile for new school structure, changed appointment language

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,7 +57,7 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation,
-                                  :school, :phone, :race, :familiar_communities, :language_one, :language_two, :language_three)
+                                  :school_id, :phone, :race, :familiar_communities, :language_one, :language_two, :language_three)
   end
 
   def set_user
